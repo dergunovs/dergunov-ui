477df3d98648bb8a5555eff0eabb1d62df9e4b9a
Alexandr Dergunov <adergunov@mtt.ru>
2021-08-11 19:07:15 +0300

    обработка клика по кнопке

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,18 +1,23 @@
+<template>
+  <div id="app">
+    <DHeader>
+      <DButton @action="test">Текст кнопки</DButton>
+    </DHeader>
+  </div>
+</template>
+
 <script>
   import Vue from "vue";
   import DButton from "../src/lib-components/button.vue";
   import DHeader from "../src/lib-components/header.vue";
 
   export default Vue.extend({
-    components: { DButton, DHeader },
     name: "ServeDev",
+    components: { DButton, DHeader },
+    methods: {
+      test() {
+        console.log(1);
+      },
+    },
   });
 </script>
-
-<template>
-  <div id="app">
-    <DHeader>
-      <DButton>Текст кнопки</DButton>
-    </DHeader>
-  </div>
-</template>
--- src/lib-components/button.vue
+++ src/lib-components/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="button">
+  <button class="button" @click="$emit('action')">
     <slot></slot>
   </button>
 </template>
