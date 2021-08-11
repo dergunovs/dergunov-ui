a85f5a7bcc46c4e46330192d72caa5e63427cb0c
Alexandr Dergunov <adergunov@mtt.ru>
2021-08-11 18:23:08 +0300

    добавил header

---
+++ src/lib-components/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <slot></slot>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "d-header",
+  };
+</script>
+
+<style>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #eee;
+    padding: 20px;
+  }
+</style>
