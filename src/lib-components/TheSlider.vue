18532aca7e6ff6507b29f4e354f766cd7accf3f4
Aleksandr Dergunov <dergunovs@mail.ru>
2021-09-09 17:35:39 +0300

    silder - основа компонента

---
+++ src/lib-components/TheSlider.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <div class="slider-container" @mousedown="slideSwipe" @touchstart="slideSwipe">
+      <div class="slider-wrapper" ref="sliderWrapper">
+        <div
+          class="slider-slide"
+          :ref="`slide-${index}`"
+          v-for="(slide, index) in slides"
+          :key="`client` + index"
+          :class="{ 'slider-slide-active': slideCurrent === index }"
+        >
+          <div class="slider-slide-arrow slider-slide-arrow-prev"></div>
+          <slot :slide="slide"></slot>
+          <div class="slider-slide-arrow slider-slide-arrow-next"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="slider-bullets">
+      <div
+        v-for="(number, index) in slides"
+        :key="`number-${index}`"
+        @click="setslideCurrent(index)"
+        class="slider-bullet"
+        :class="{ 'slider-bullet-active': slideCurrent === index }"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TheSlider",
+
+    data() {
+      return {
+        slideCurrent: 0,
+        slideWidth: NaN,
+      };
+    },
+
+    props: {
+      slides: { type: Array, required: true },
+    },
+
+    methods: {
+      setslideCurrent(index) {
+        this.$refs.sliderWrapper.style.transform = `translate3d(-${index * this.slideWidth}px, 0px, 0px)`;
+        this.slideCurrent = index;
+      },
+      slideSwipe(e) {
+        let slideClickZone = e.offsetX / this.slideWidth;
+
+        if (slideClickZone < 0.2 && this.slideCurrent !== 0) {
+          this.prevSlide();
+        }
+        if (slideClickZone > 0.8 && this.slideCurrent !== this.slides.length - 1) {
+          this.nextSlide();
+        }
+      },
+      nextSlide() {
+        this.slideCurrent = this.slideCurrent + 1;
+        this.$refs.sliderWrapper.style.transform = `translate3d(-${this.slideCurrent * this.slideWidth}px, 0px, 0px)`;
+      },
+      prevSlide() {
+        this.slideCurrent = this.slideCurrent - 1;
+        this.$refs.sliderWrapper.style.transform = `translate3d(-${this.slideCurrent * this.slideWidth}px, 0px, 0px)`;
+      },
+      updateSlideWidth() {
+        this.slideWidth = this.$refs["slide-" + this.slideCurrent][0].offsetWidth;
+      },
+    },
+
+    mounted() {
+      this.slideWidth = this.$refs["slide-0"][0].offsetWidth;
+      window.addEventListener("resize", this.updateSlideWidth);
+    },
+
+    beforeDestroy() {
+      window.removeEventListener("resize", this.updateSlideWidth);
+    },
+  };
+</script>
+
+<style>
+  .slider-container {
+    position: relative;
+    overflow: hidden;
+  }
+  .slider-wrapper {
+    position: relative;
+    overflow: visible;
+    display: flex;
+    user-select: none;
+    width: 100%;
+    height: 100%;
+    transition-property: transform;
+    transform: translate3d(0px, 0px, 0px);
+    will-change: transform;
+    transition-property: transform;
+    transition-duration: 600ms;
+  }
+  .slider-slide {
+    width: 100%;
+    position: relative;
+    transition-property: transform;
+    flex-shrink: 0;
+    transition-duration: 600ms;
+    z-index: 1;
+    pointer-events: none;
+  }
+
+  .slider-bullets {
+    display: flex;
+    justify-content: center;
+    margin-top: 24px;
+  }
+  .slider-bullet {
+    margin: 4px;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    cursor: pointer;
+    transition-duration: 300ms;
+    background-color: var(--color-gray-light);
+  }
+  .slider-bullet:hover {
+    background-color: var(--color-gray);
+  }
+  .slider-bullet-active {
+    background-color: var(--color-primary);
+  }
+  .slider-bullet-active:hover {
+    background-color: var(--color-primary-dark);
+  }
+</style>
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,7 +1,8 @@
 export { default as TheButton } from "./TheButton.vue";
-export { default as TheList } from "./TheList.vue";
-export { default as TheIcon } from "./TheIcon.vue";
-export { default as TheCode } from "./TheCode.vue";
-export { default as TheInput } from "./TheInput.vue";
-export { default as TheMap } from "./TheMap.vue";
 export { default as TheCard } from "./TheCard.vue";
+export { default as TheCode } from "./TheCode.vue";
+export { default as TheIcon } from "./TheIcon.vue";
+export { default as TheInput } from "./TheInput.vue";
+export { default as TheList } from "./TheList.vue";
+export { default as TheMap } from "./TheMap.vue";
+export { default as TheSlider } from "./TheSlider.vue";
