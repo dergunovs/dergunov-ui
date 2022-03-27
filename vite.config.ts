73806bba707aae613c9072b14b8b78e0dccf5272
Aleksandr Dergunov <dergunovs@mail.ru>
2022-03-27 17:31:03 +0300

    добавил dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      cleanVueFileName: true,
+      outputDir: "./dist/types",
+      copyDtsFiles: false,
+    }),
+  ],
 
   build: {
     target: "esnext",
