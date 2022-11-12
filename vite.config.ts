6996e3cb8cfa96cd107d0c17509aa0b4bc5ef84c
Aleksandr Dergunov <dergunovs@mail.ru>
2022-11-12 15:03:30 +0300

    fix: types generation

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [vue(), dts({ cleanVueFileName: true, outputDir: "./dist/types", copyDtsFiles: false })],
+  plugins: [
+    vue(),
+    dts({ cleanVueFileName: true, outputDir: "./dist/types", insertTypesEntry: true, copyDtsFiles: false }),
+  ],
 
   build: {
     target: "esnext",
